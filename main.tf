506f3751b892c9d518add9595d181cdd8e578a35
sneha dushad <snehadushad27@gmail.com>
2026-08-09 12:10:57 +0530

    RGcode

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = ""
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-terraform-demo"
+    location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name                     = "stterraformdemo"
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
